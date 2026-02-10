f522b3ff359fe96884a7f804b3acc51030d5b6ad
Hifza-Abbasi <hifzaabbasi688@gmail.com>
2026-02-10 17:24:06 +0500

    changing the color of text area in style css file change done by Hifzaabbasi

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,4 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 export default bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  .catch((error) => console.error(error));
